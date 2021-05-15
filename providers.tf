9c2fdfd02b8e6b6eec97171fa5705bedc7e9b45b
Ademilson Junior <ademilson.junior@aluno.faculdadeimpacta.com.br>
2021-05-15 12:31:32 -0300

    Convertendo Credential em Variável

--- providers.tf
+++ providers.tf
@@ -10,8 +10,6 @@ terraform {
 # Configura o Provider Google Cloud com o Projeto
 provider "google" {
 
-  credentials = file("/home/vagrant/lab-devops-cloudjr-08adb79770b8.json")
-
   project = "lab-devops-cloudjr"
   region  = "us-east1"
   zone    = "us-east1-b"
